8de996d84b792cb3cc6eed4f448879c21df31698
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2024-05-22 13:26:25 -0300

    Post Blog adicionado: Subindo um serviço de monitoria com prometheus e grafana

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716237968',resources: [
+    cacheName: 'chirpy-1716395131',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
