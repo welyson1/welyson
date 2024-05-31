974c52372503da30bc6065956f5591da2dc365ba
Lorenzo Uriel <lorenzo.uriel@geosiga.com.br>
2024-05-31 11:12:28 -0300

    Add Vagrant to Blog - README corrected

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716395131',resources: [
+    cacheName: 'chirpy-1717164715',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
